9d514bfccf36e696dae9fc493dd6618d3a6f47b9
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-02-25 12:23:50 +0000

    native build success

---
+++ spring-boot-native-samples-web/src/test/java/io/samples/web/SomeTest.java
@@ -0,0 +1,20 @@
+package io.samples.web;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author: baoxin.zhao
+ * @date: 2024/2/25
+ */
+@Slf4j
+public class SomeTest {
+
+    @Test
+    void test() {
+        log.info("=======================Test=======================");
+    }
+
+
+}
