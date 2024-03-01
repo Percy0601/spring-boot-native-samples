b0bb27235d69c9cc4e7e5f99ce1202ad7878cb21
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-03-01 02:03:57 +0000

    config redis

--- spring-boot-native-samples-redis/src/main/java/io/samples/redis/controller/RedisController.java
+++ spring-boot-native-samples-redis/src/main/java/io/samples/redis/controller/RedisController.java
@@ -1,5 +1,13 @@
 package io.samples.redis.controller;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import org.redisson.api.RedissonClient;
+import org.redisson.client.RedisClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +21,19 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/redis")
 public class RedisController {
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    @GetMapping("/key/put")
+    public String putKey(String user) {
+        redisTemplate.opsForValue().set(user, UUID.randomUUID(), 1, TimeUnit.MINUTES);
+        return "OK";
+    }
+
+    @GetMapping("/key/get")
+    public String getKey(String user) {
+        String result = redisTemplate.opsForValue().get(user).toString();
+        return result;
+    }
 
 }
