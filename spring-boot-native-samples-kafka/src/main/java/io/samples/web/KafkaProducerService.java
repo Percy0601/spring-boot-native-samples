84cb7b3450f44b25e845fa89f6e7d32b27dd0466
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-07-28 12:22:08 +0100

    kafka

---
+++ spring-boot-native-samples-kafka/src/main/java/io/samples/web/KafkaProducerService.java
@@ -0,0 +1,47 @@
+package io.samples.web;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author: baoxin.zhao
+ * @date: 2024/7/28
+ */
+@Slf4j
+@Service
+public class KafkaProducerService {
+
+    @Value("${kafka.host:192.168.1.16:9092}")
+    private String host;
+    @Value("${kafka.topic:topics-test}")
+    private String topic;
+
+    private KafkaProducer<String, String> kafkaProducer;
+
+    @PostConstruct
+    private void init() {
+        Properties p = new Properties();
+        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, host);//kafka地址，多个地址用逗号分割
+        p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        kafkaProducer = new KafkaProducer<>(p);
+    }
+
+    public Future<RecordMetadata> send(String message){
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
+        Future<RecordMetadata> result = kafkaProducer.send(record);
+        return result;
+    }
+
+}
---
+++ spring-boot-native-samples-kafka/src/test/java/io/samples/web/SomeTest.java
@@ -0,0 +1,42 @@
+package io.samples.web;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author: baoxin.zhao
+ * @date: 2024/2/25
+ */
+@Slf4j
+public class SomeTest {
+
+    @Test
+    void test() {
+//        Properties props = new Properties();
+//        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.1.16:9092");
+//        props.put(AdminClientConfig.RETRIES_CONFIG, 0);
+//        props.put("batch.size", 16384);
+//        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        AdminClient create = KafkaAdminClient.create(props);
+//        List<NewTopic> newTopics = new ArrayList<>();
+//        NewTopic newTopic = new NewTopic("test-topic", 1, (short)1);
+//        newTopics.add(newTopic);
+//        create.createTopics(newTopics);
+//        create.close();
+    }
+
+}
