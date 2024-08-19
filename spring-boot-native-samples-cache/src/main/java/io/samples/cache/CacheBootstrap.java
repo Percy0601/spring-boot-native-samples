9329f83e6ee2d3a591c219f782ae6a3259c9bfe7
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-08-19 02:57:54 +0100

    spring-boot cache

---
+++ spring-boot-native-samples-cache/src/main/java/io/samples/cache/CacheBootstrap.java
@@ -0,0 +1,15 @@
+package io.samples.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: baoxin.zhao
+ * @date: 2024/2/27
+ */
+@SpringBootApplication
+public class CacheBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(CacheBootstrap.class, args);
+    }
+}
---
+++ spring-boot-native-samples-cache/src/main/java/io/samples/cache/controller/RedisController.java
@@ -0,0 +1,19 @@
+package io.samples.cache.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author: baoxin.zhao
+ * @date: 2024/2/27
+ */
+@Slf4j
+@RestController
+@RequestMapping("/cache")
+public class RedisController {
+
+
+
+}
