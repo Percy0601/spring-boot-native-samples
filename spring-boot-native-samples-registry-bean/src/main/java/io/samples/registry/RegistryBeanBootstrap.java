3e5f6848a439820ff20e6f466033fd1135eeb4f3
baoxin.zhao <baoxin.zhao@yiling.cn>
2024-10-17 15:18:11 +0100

    regitry bean init

---
+++ spring-boot-native-samples-registry-bean/src/main/java/io/samples/registry/RegistryBeanBootstrap.java
@@ -0,0 +1,17 @@
+package io.samples.registry;
+
+/**
+ * @author: baoxin.zhao
+ * @date: 2024/02/24
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryBeanBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryBeanBootstrap.class, args);
+    }
+
+}
