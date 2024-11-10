2e50a007619476018f72ca43aac7a41d58fff258
Percy0601 <2008dreams@2008.sina.com>
2024-11-10 09:47:05 +0800

    advanced

---
+++ spring-boot-native-samples-advanced/src/main/java/io/samples/web/config/AdvanceConfig.java
@@ -0,0 +1,23 @@
+package io.samples.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.samples.web.advanced.AdvancedBeanPostProcessor;
+
+/**
+ * @author: baoxin.zhao
+ * @date: 2024/11/10
+ */
+
+@Configuration
+public class AdvanceConfig {
+
+    @Bean
+    public AdvancedBeanPostProcessor advancedBeanPostProcessor() {
+        return new AdvancedBeanPostProcessor();
+    }
+
+
+
+}
