bc4e54a12ff232895c91dc5a058d67678deddf3b
Percy0601 <745356640@qq.com>
2025-09-13 16:39:38 +0800

    advanced

---
+++ spring-boot-native-samples-advanced/src/main/java/io/samples/web/annotation/AdvancedAnnotation.java
@@ -0,0 +1,11 @@
+package io.samples.web.annotation;
+
+/**
+ * 
+ * @author: baoxin.zhao
+ * @date: 2024/11/17 
+ */
+
+
+public @interface AdvancedAnnotation {
+}
---
+++ spring-boot-native-samples-advanced/src/main/java/io/samples/web/service/AdvancedService.java
@@ -0,0 +1,11 @@
+package io.samples.web.service;
+
+/**
+ * 
+ * @author: baoxin.zhao
+ * @date: 2024/11/17 
+ */
+
+
+public class AdvancedService {
+}
