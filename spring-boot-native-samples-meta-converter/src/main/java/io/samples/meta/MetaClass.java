53696ffbaa253d9b3aeef95e73238ff74d73d280
Percy0601 <745356640@qq.com>
2026-02-26 18:00:30 +0800

    meta converter

--- spring-boot-native-samples-meta-converter/src/main/java/io/samples/meta/MetaClass.java
+++ spring-boot-native-samples-meta-converter/src/main/java/io/samples/meta/MetaClass.java
@@ -2,11 +2,14 @@ package io.samples.meta;
 
 import java.util.List;
 
+import lombok.Data;
+
 /**
  *
  * @author: baoxin.zhao
  * @date: 2026/2/26
  */
+@Data
 public class MetaClass {
     private String name;
     private List<MetaProperty> properties;
--- spring-boot-native-samples-meta-converter/src/main/java/io/samples/meta/MetaProperty.java
+++ spring-boot-native-samples-meta-converter/src/main/java/io/samples/meta/MetaProperty.java
@@ -1,10 +1,13 @@
 package io.samples.meta;
 
+import lombok.Data;
+
 /**
  *
  * @author: baoxin.zhao
  * @date: 2026/2/26
  */
+@Data
 public class MetaProperty {
     private String name;
     private Object value;
