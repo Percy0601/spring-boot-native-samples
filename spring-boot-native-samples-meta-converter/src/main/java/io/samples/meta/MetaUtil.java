bb0d7b55ec2088ba295de8f0cba17bfbd3f92e22
Percy0601 <745356640@qq.com>
2026-03-01 21:46:21 +0800

    finish

--- spring-boot-native-samples-meta-converter/src/main/java/io/samples/meta/MetaUtil.java
+++ spring-boot-native-samples-meta-converter/src/main/java/io/samples/meta/MetaUtil.java
@@ -3,11 +3,13 @@ package io.samples.meta;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
+import org.springframework.beans.BeanUtils;
 
 /**
  *
@@ -34,10 +36,8 @@ public class MetaUtil {
 
         MetaClass metaClass = loadClass(namespace, name);
         Object document = Configuration.defaultConfiguration().jsonProvider().parse(json);
-
-
-
-        return null;
+        handleMetaClass(metaClass, document);
+        return metaClass;
     }
 
     public static MetaCollection metaCollection(String type, String json) {
@@ -62,11 +62,44 @@ public class MetaUtil {
                 } else {
                     typeNameNamespace = "default";
                 }
-                loadClass(typeNameNamespace, typeName);
+                MetaClass mc = loadClass(typeNameNamespace, typeName);
+                metaProperty.setValue(mc);
 
+                handleMetaClass(mc, value);
                 continue;
             }
             if(metaProperty.getType().equals(MetaCollection.class.getName())) {
+                String name = metaProperty.getName();
+                String namePath = "$.".concat(name);
+                Object value = JsonPath.read(document, namePath);
+                if(null == value) {
+                    continue;
+                }
+
+                String typeName = metaProperty.getTypeName();
+                String typeNameNamespace = "";
+                if(typeName.contains(".")) {
+                    String[] parts = typeName.split(".");
+                    typeName = parts[parts.length -1];
+                } else {
+                    typeNameNamespace = "default";
+                }
+                MetaClass mc = loadClass(typeNameNamespace, typeName);
+
+                List<Map<String, Object>> vs = (List<Map<String, Object>>)value;
+                List<MetaClass> mcs = new ArrayList<>();
+
+                for(Map<String, Object> v: vs) {
+                    MetaClass m = new MetaClass();
+                    BeanUtils.copyProperties(mc, m);
+                    mcs.add(m);
+                }
+                metaProperty.setValue(mcs);
+
+                int i = 0;
+                for(MetaClass m: mcs) {
+                    handleMetaClass(m, vs.get(i++));
+                }
 
                 continue;
             }
@@ -140,8 +173,8 @@ public class MetaUtil {
         }
         MetaClass metaClass = new MetaClass();
         mapping.put(fullPath, metaClass);
-        if(name.equals("Store")) {
-            metaClass.setName("Store");
+        if(name.equals("Market")) {
+            metaClass.setName("Market");
             metaClass.setNamespace("default");
             List<MetaProperty> properties = new ArrayList<>();
             MetaProperty expensiveProperty = new MetaProperty();
@@ -150,16 +183,11 @@ public class MetaUtil {
             properties.add(expensiveProperty);
 
             MetaProperty bicycleProperty = new MetaProperty();
-            bicycleProperty.setName("bicycle");
+            bicycleProperty.setName("store");
             bicycleProperty.setType(MetaClass.class.getName());
-            bicycleProperty.setTypeName("Bicycle");
+            bicycleProperty.setTypeName("Store");
             properties.add(bicycleProperty);
 
-            MetaProperty bookProperty = new MetaProperty();
-            bookProperty.setName("book");
-            bookProperty.setType(MetaCollection.class.getName());
-            bookProperty.setTypeName("Book");
-            properties.add(bookProperty);
             metaClass.setProperties(properties);
             return metaClass;
         }
@@ -191,6 +219,29 @@ public class MetaUtil {
             property4.setName("price");
             property4.setType(Double.class.getName());
             properties.add(property4);
+
+            metaClass.setProperties(properties);
+            return metaClass;
+        }
+
+        if(name.equals("Store")) {
+            metaClass.setName("Store");
+            metaClass.setNamespace("default");
+//            "color": "red",
+//                    "price": 19.958876567765,
+//                    "count": 3
+            List<MetaProperty> properties = new ArrayList<>();
+            MetaProperty property1 = new MetaProperty();
+            property1.setName("book");
+            property1.setType(MetaCollection.class.getName());
+            property1.setTypeName("Book");
+            properties.add(property1);
+
+            MetaProperty property2 = new MetaProperty();
+            property2.setName("bicycle");
+            property2.setTypeName("Bicycle");
+            property2.setType(MetaClass.class.getName());
+            properties.add(property2);
 
             metaClass.setProperties(properties);
             return metaClass;
--- spring-boot-native-samples-meta-converter/src/test/java/io/samples/meta/test/MetaSomeTest.java
+++ spring-boot-native-samples-meta-converter/src/test/java/io/samples/meta/test/MetaSomeTest.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 import com.jayway.jsonpath.DocumentContext;
+import io.samples.meta.MetaClass;
+import io.samples.meta.MetaUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
@@ -12,6 +14,7 @@ import org.springframework.util.PathMatcher;
 
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
+import tools.jackson.databind.ObjectMapper;
 
 @Slf4j
 public class MetaSomeTest {
@@ -40,7 +43,7 @@ public class MetaSomeTest {
         String author1 = JsonPath.read(document, "$.store.book[1].author");
 
         log.info("author-0:{}", author0);
-        Object book0 = JsonPath.read(document, "$.store.book").toString();
+        Object book0 = JsonPath.read(document, "$.store.book");
         log.info("book-0:{}", book0);
         Object bicycle0 = JsonPath.read(document, "$.store.bicycle");
         log.info("bicycle-0:{}", bicycle0);
@@ -56,5 +59,22 @@ public class MetaSomeTest {
         boolean result = matcher.match("石家庄以岭药业股份有限公司/营销公司/配方颗粒事业部/营销部/新业务部/**", fullpath);
 
         log.info("result:{}", result);
+    }
+
+    @Test
+    void testMetaClass() {
+        ClassPathResource classPathResource = new ClassPathResource("./222.json");
+        String json = "";
+        try {
+            json = Files.readString(classPathResource.getFilePath());
+            //log.info("========{}", json);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Object document = Configuration.defaultConfiguration().jsonProvider().parse(json);
+        MetaClass metaClass = MetaUtil.metaClass("Market", json);
+        ObjectMapper mapper = new ObjectMapper();
+        log.info("=========={}", mapper.writeValueAsString(metaClass));
     }
 }
